2def84cc1c2bfe256d965b2d5bf91150243f5fd4
Mohan <mohankumaronly81@gmail.com>
2025-09-09 18:17:51 +0530

    home frontend updates done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,8 +13,8 @@ const Navbar = () => {
                     <a href="#" className='px-4 font-semibold'>Contact</a>
                 </div>
                 <div>
-                    <button className='mx-3'>Login</button>
-                    <button className='bg-black text-white px-3 py-1 rounded-2xl mx-3'>SignUp</button>
+                    <button className='mx-3 cursor-pointer'>Login</button>
+                    <button className='bg-black text-white px-3 py-1 rounded-2xl mx-3 cursor-pointer'>SignUp</button>
                 </div>
             </div>
         </div>
